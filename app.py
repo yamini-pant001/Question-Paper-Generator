ffa0f4c8d885e5edaaac1f46be5e3bba136d2a52
yamini <yaminipant001@gmail.com>
2026-02-11 11:35:45 +0530

    user input added

---
+++ app.py
@@ -0,0 +1,8 @@
+print("Welcome to Question Paper Generator System")
+
+name = input("Enter Your Name ")
+subject = input("Enter Subject Name ")
+
+print("Generating Question Paper....")
+print("Welcome",name)
+print("Your Subject :",subject)
